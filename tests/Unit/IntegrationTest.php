d8c51938697b93a8a035a10742c2d5d924933fa3
Mustafa UYSAL <mustafa-uysal@outlook.com>
2024-06-19 00:50:46 +0300

    test v2

--- tests/Unit/IntegrationTest.php
+++ tests/Unit/IntegrationTest.php
@@ -19,6 +19,7 @@ use Tests\MigrateFreshSeedOnce;
 class IntegrationTest extends TestCase
 {
 
+    use DatabaseTransactions;
     public function setUp(): void
     {
         parent::setUp();
@@ -33,7 +34,7 @@ class IntegrationTest extends TestCase
         // Veritabanını sıfırlamak için migrasyonları tekrar çalıştırın
 
     }
-    public function test_can_create_integration()
+    /*public function test_can_create_integration()
     {
 
         $integrationArr = ['hepsiburada', 'getir', 'trendyol'];
@@ -64,6 +65,70 @@ class IntegrationTest extends TestCase
         ];
 
         $this->postJson(route('integration.store'), $data, $headers)
+            ->assertStatus(201);
+    }*/
+
+    public function test_can_update_integration()
+    {
+
+        $integrationArr = ['hepsiburada', 'getir', 'trendyol'];
+        $index = rand(0 , count($integrationArr) -1);
+
+        $user = User::factory()->create();
+
+        $clientRepository = new ClientRepository();
+
+        // Passport istemcisini oluşturun
+        $client = $clientRepository->createPersonalAccessClient(
+            $user->id,
+            'Test Client', // İstemci adı
+            'http://localhost' // İstemci URL'si
+        );
+
+        $token = $user->createToken('API TOKEN')->accessToken;
+
+        $headers = [
+            'Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
+        ];
+
+        $data = [
+            'integration' => $integrationArr[$index],
+            'username' => $this->faker->word,
+            'password' => $this->faker->word,
+            'id' => 1
+        ];
+
+        $this->postJson(route('integration.update'), $data, $headers)
+            ->assertStatus(201);
+    }
+
+    public function test_can_delete_integration()
+    {
+
+        $user = User::factory()->create();
+
+        $clientRepository = new ClientRepository();
+
+        // Passport istemcisini oluşturun
+        $client = $clientRepository->createPersonalAccessClient(
+            $user->id,
+            'Test Client', // İstemci adı
+            'http://localhost' // İstemci URL'si
+        );
+
+        $token = $user->createToken('API TOKEN')->accessToken;
+
+        $headers = [
+            'Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
+        ];
+
+        $data = [
+            'id' => 1
+        ];
+
+        $this->postJson(route('integration.delete'), $data, $headers)
             ->assertStatus(201);
     }
 
